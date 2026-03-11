9feb9c4ab077c55eb82a9b4ace3010de58173506
Oleksii <borodchenko@gmail.com>
2026-03-11 10:42:06 +0200

    refactor css

--- js/account-pages.js
+++ js/account-pages.js
@@ -18,42 +18,6 @@ function initAccountDropdown() {
     });
 }
 
-function initSigninFlow() {
-    var $forgotEmailToggle = $("#forgot-email-toggle");
-    var $defaultContent = $("#signin-default-content");
-    var $validEmailContent = $("#signin-valid-email-content");
-    var $confirmContent = $("#signin-confirm-content");
-    var $signinEmailInput = $("#signin-email");
-    var $validEmailInput = $("#valid-email");
-    var $validEmailForm = $("#valid-email-form");
-    var $confirmEmailText = $("#confirm-email-text");
-
-    if (!$forgotEmailToggle.length || !$defaultContent.length || !$validEmailContent.length || !$confirmContent.length || !$validEmailForm.length) return;
-
-    $forgotEmailToggle.on("click", function (e) {
-        e.preventDefault();
-        $defaultContent.prop("hidden", true);
-        $validEmailContent.prop("hidden", false);
-
-        if ($signinEmailInput.length && $validEmailInput.length && $signinEmailInput.val()) {
-            $validEmailInput.val($signinEmailInput.val());
-        }
-
-        if ($validEmailInput.length) {
-            $validEmailInput.trigger("focus");
-        }
-    });
-
-    $validEmailForm.on("submit", function (e) {
-        e.preventDefault();
-        $validEmailContent.prop("hidden", true);
-        $confirmContent.prop("hidden", false);
-
-        if ($confirmEmailText.length && $validEmailInput.length && $validEmailInput.val()) {
-            $confirmEmailText.text($validEmailInput.val());
-        }
-    });
-}
 
 function initPasswordToggle() {
     $(".account-form__toggle").on("click", function () {
@@ -68,22 +32,8 @@ function initPasswordToggle() {
     });
 }
 
-function initSignupForm() {
-    var $signupForm = $(".account-form_signup");
-    var $signupSuccess = $(".account-card_success");
-
-    if (!$signupForm.length || !$signupSuccess.length) return;
-
-    $signupForm.on("submit", function (e) {
-        e.preventDefault();
-        $signupForm.prop("hidden", true);
-        $signupSuccess.prop("hidden", false);
-    });
-}
 
 $(document).ready(function () {
     initAccountDropdown();
-    initSigninFlow();
     initPasswordToggle();
-    initSignupForm();
 });
